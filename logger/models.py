7d3c024ffd1dfb8179ced8dd2d23d875ffd797b2
Jonathan Le Bonzec <jonathan.lucky13@gmail.com>
2016-10-23 01:01:15 +0200

    Changed model
    - Uses the date functions instead of their values
    - Added index on several fields
    - Added str methods
    - Added an attack type (ranged, melee) field for roles

--- logger/models.py
+++ logger/models.py
@@ -1,6 +1,5 @@
-import datetime
-
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -12,24 +11,40 @@ class Role(models.Model):
         (_physical, 'Physical'),
         (_magical, 'Magical')
     )
-    name = models.CharField(max_length=30, unique=True)
+    _ranged = 'R'
+    _melee = 'M'
+    _attack_type = (
+        (_ranged, 'Ranged'),
+        (_melee, 'Melee')
+    )
+    name = models.CharField(max_length=30, unique=True, db_index=True)
     damage = models.CharField(
         max_length=1,
         choices=_damage_type,
         default=_physical,
     )
-    # TODO: how to set name as primary index?
+    attack = models.CharField(
+        max_length=1,
+        choices=_attack_type,
+        default=_melee
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Pantheon(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    date_release = models.DateField(default=datetime.date.today())
+    name = models.CharField(max_length=30, unique=True, db_index=True)
+    date_release = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
 
 
 class God(models.Model):
     # TODO: set several fields as index
-    name = models.CharField(max_length=60, unique=True)
-    date_release = models.DateField(datetime.date.today())
+    name = models.CharField(max_length=60, unique=True, db_index=True)
+    date_release = models.DateField(timezone.now)
     role = models.ForeignKey(
         Role,
         on_delete=models.CASCADE
@@ -39,6 +54,9 @@ class God(models.Model):
         on_delete=models.CASCADE
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Match(models.Model):
     _win = 'W'
@@ -47,15 +65,16 @@ class Match(models.Model):
         (_win, 'Win'),
         (_loss, 'Loss')
     )
-    self = models.ForeignKey(God, related_name='self')
-    opponent = models.ForeignKey(God, related_name='opponent')
+    self = models.ForeignKey(God, related_name='self', db_index=True)
+    opponent = models.ForeignKey(God, related_name='opponent', db_index=True)
     result = models.CharField(
         max_length=1,
         choices=_results,
-        default=_win
+        default=_win,
+        db_index=True
     )
     date = models.DateTimeField(
-        default=datetime.datetime.now()
+        default=timezone.now
     )
     kills = models.IntegerField()
     deaths = models.IntegerField()

3e4ca2642bf3d97739559895fcf49d54442f4580
Jonathan Le Bonzec <jonathan.lucky13@gmail.com>
2016-10-29 13:02:58 +0200

    Added template inheritance and a login page

--- logger/urls.py
+++ logger/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # ex: /logger
     url(r'^$', views.index, name='index'),
+    url(r'^login$', views.connect, name='login'),
     url(r'^log', views.log, name='log')
 ]
--- logger/views.py
+++ logger/views.py
@@ -4,9 +4,12 @@ from django.shortcuts import (
     HttpResponseRedirect,
     resolve_url
 )
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
 from logger.forms import MatchForm
+
 
 # Create your views here.
 
@@ -18,11 +21,24 @@ def index(request):
         return HttpResponse("Boo, I don't know you!")
 
 
+def connect(request):
+    auth_form = AuthenticationForm()
+    if request.method == 'POST':
+        auth_form = AuthenticationForm(request.POST)
+        if auth_form.is_valid():
+            username = auth_form.username
+            password = auth_form.password
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                return HttpResponseRedirect(resolve_url('index'))
+    return HttpResponse(auth_form)
+
+
+@login_required
 def log(request):
     match_form = MatchForm()
     if request.method == "POST":
         match_form = MatchForm(request.POST)
-        print(match_form.as_p())
         if match_form.is_valid():
             match_form.save()
             return HttpResponseRedirect(resolve_url('index'))
